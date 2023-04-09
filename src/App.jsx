c89d8cae9e3eb6fb2e6827dc2056b2c2d2370f1c
arafataft <arafatcd@gmail.com>
2023-04-10 04:20:05 +0600

    featured jobs half done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import JobCategory from './components/JobCategory/JobCategory';
+import FeaturedJobs from './components/FeaturedJobs/FeaturedJobs';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <NavBar></NavBar>
     <Banner></Banner>
     <JobCategory></JobCategory>
+    <FeaturedJobs></FeaturedJobs>
     </>
   );
 }
---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Button} from 'react-bootstrap';
+
+
+const FeaturedJobs = () => {
+    const [jobs,setJobs]=useState([])
+
+    useEffect(()=>{
+        fetch("data1.json")
+        .then((res)=>res.json())
+        .then((data)=>setJobs(data));
+    },[])
+    return (
+        <div>
+            <div className="py-5 bg-light">
+      <div className="container py-5">
+        <div className="mb-5 text-center">
+        <h2 >Featured Jobs</h2>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+        <div className="row">
+          {
+            jobs.map(job=>(<div key={job.id} className="col-md-6 mb-4">
+            <Card>
+              <Card.Img className='m-4' style={{ width: 200, height: 60 }} src={'https://picsum.photos/500/500?random=2'} />
+              <Card.Body >
+                <Card.Title>{job.position}</Card.Title>
+                <Card.Text>
+                  {job.company}
+                </Card.Text>
+                <Button variant="primary">View Details</Button>
+              </Card.Body>
+            </Card>
+          </div>))
+          }
+        <div className='text-center'><Button variant="primary">Sea More</Button></div>
+
+         
+        </div>
+      </div>
+    </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
